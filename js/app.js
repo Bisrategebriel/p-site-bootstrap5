32291a45d689dd5e796d359b3b3361db8924d499
Bisrategebriel Fisseha <bisrate22@gmail.com>
2022-03-07 00:48:56 +0300

    Add portfolio section slider

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// swiper js
+const swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
